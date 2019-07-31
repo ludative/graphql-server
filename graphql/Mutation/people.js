9847f51b17588a01f0c3faf0f8ec46304e510bf6
Saemii <wrfg12@gmail.com>
2019-07-31 09:09:47 +0900

    add sequelize & mysql2 / sequelize 연동해서 user 리스트뿌림

--- graphql/Mutation/people.js
+++ graphql/Mutation/people.js
@@ -1,9 +1,13 @@
-import { addPerson } from '../db'
+import models from '../models'
 
 const create = (_, { name, age, gender }) => addPerson(name, age, gender)
 
+// const addPerson = async () => {
+//     const user = await models.User.create
+// }
+
 const peopleMutation = {
-    create
-};
+  create
+}
 
 export default peopleMutation
--- graphql/db.js
+++ graphql/db.js
@@ -1,70 +1,68 @@
-export let People = [
-  {
-    id: '0',
-    name: 'Nicolas',
-    age: 18,
-    gender: 'female'
-  },
-  {
-    id: '1',
-    name: 'Jisu',
-    age: 18,
-    gender: 'female'
-  },
-  {
-    id: '2',
-    name: 'Japan Guy',
-    age: 18,
-    gender: 'male'
-  },
-  {
-    id: '3',
-    name: 'Daal',
-    age: 18,
-    gender: 'male'
-  },
-  {
-    id: '4',
-    name: 'JD',
-    age: 18,
-    gender: 'male'
-  },
-  {
-    id: '5',
-    name: 'moondaddi',
-    age: 18,
-    gender: 'male'
-  },
-  {
-    id: '6',
-    name: 'flynn',
-    age: 18,
-    gender: 'male'
-  }
-]
+import bcrypt from 'bcrypt-nodejs'
+import models from './models'
+import config from '../config'
 
-export const getById = id => {
-  const filteredPeople = People.filter(person => person.id === String(id))
-  return filteredPeople[0]
+export default async () => {
+  try {
+    await models.sequelize.sync({
+      force: config.db.forceSync,
+      alter: config.db.alter
+    })
+
+    if (config.db.forceSync) {
+      await initialize()
+    }
+    console.log('db synced!')
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-export const addPerson = (name, age, gender) => {
-  const newPerson = {
-    id: `${People.length + 1}`,
-    name,
-    age,
-    gender
+const initialize = async function() {
+  /**
+   * 사용자 생성
+   */
+  try {
+    // Users
+    await Promise.all(modelsToInit.users.map(user => models.User.create(user)))
+    // Posts
+    await Promise.all(modelsToInit.posts.map(post => models.Post.create(post)))
+  } catch (err) {
+    throw err
   }
-  People.push(newPerson)
-  return newPerson
 }
 
-export const deletePerson = id => {
-  const cleanPerson = People.filter(person => person.id !== String(id))
-  if (People.length > cleanPerson.length) {
-    people = cleanPerson
-    return true
-  } else {
-    return false
-  }
+const hash = bcrypt.hashSync('1234')
+
+const modelsToInit = {
+  users: [
+    {
+      id: 1,
+      username: 'admin',
+      password: hash,
+      nickname: '테스트 관리자',
+      isAdmin: true
+    },
+    {
+      id: 2,
+      username: 'user',
+      password: hash,
+      nickname: '테스트 사용자'
+    }
+  ],
+
+  posts: [
+    {
+      id: 1,
+      title: '테스트 포스트 1',
+      content: '이것은 테스트용 포스트입니다.',
+      UserId: 1
+    },
+    {
+      id: 2,
+      title: '테스트 포스트 2',
+      content: '이것은 테스트용 두번재 포스트입니다.',
+      UserId: 2
+    }
+  ]
 }
